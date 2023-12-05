da9b438070486af1140ae8f365f29d64c84033d5
Eric Arizmendi <141188098+AsesoraIT@users.noreply.github.com>
2023-12-05 15:32:39 -0600

    Update __manifest__.py

--- sale_order_automation-ASESORAIT/sale_order_automation/__manifest__.py
+++ sale_order_automation-ASESORAIT/sale_order_automation/__manifest__.py
@@ -3,18 +3,20 @@
 {
     'name' : 'Sale Order Automation',
     'version' : '1.0',
+    'depends' : ['sale_stock'],
     'author':'AsesoraIT',
-    'category': 'Sales',
     'summary': """Enable auto sale workflow with sale order confirmation.""",
     'description': """
 
         You can directly create invoice and set done to delivery order by single click
 
     """,
+    "price": 0,    
+    "currency": "USD",
+    'category': 'Sales',
     'website': '',
     'license': 'LGPL-3',
-    'support':'',
-    'depends' : ['sale_stock'],
+    "website" : "",
     'data': [
         'views/stock_warehouse.xml',
     ],
