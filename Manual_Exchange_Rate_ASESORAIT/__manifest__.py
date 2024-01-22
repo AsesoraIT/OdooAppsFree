6b41ba07c1edec9da2e925c5a6b302891646d380
Eric Arizmendi <141188098+AsesoraIT@users.noreply.github.com>
2024-01-22 13:55:40 -0600

    Add files via upload

---
+++ Manual_Exchange_Rate_ASESORAIT/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    "name" : "Manual Currency Exchange Rate",
+    "version" : "1.0",
+    "depends" : ['base','account'],
+    "author": "AsesoraIT",
+    "summary" : 'Apply Manual Exchange Rate on Invoices and Register Payment.',
+    "description" : 'Apply Manual Exchange Rate on Invoices and Register Payment.',
+    "price": '5.0',    
+    "currency": "USD",
+    'category': 'Accounting',
+    "website" : "",
+    "data" :[
+        "views/customer_invoice.xml",
+        "views/account_payment_view.xml",
+        ],
+    #'qweb':[
+    #],
+    "auto_install": False,
+    "installable": True,
+    #'application': True,
+    "images": ['static/description/icon.png'],
+    "license": "LGPL-3",
+}
